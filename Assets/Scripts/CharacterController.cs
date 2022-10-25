e116d9429557c6c915a0193e443140a4e942505c
W412 <w412@hjuhsd.k12.ca.us>
2022-10-25 15:37:28 -0700

    Sliding (beta)

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,11 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class CharacterController : MonoBehaviour
 {
 
+    // Animator
+    private GameObject animator;
+
+    // Character Collider
+    [SerializeField] private BoxCollider2D characterCollider;
+    private float walkingHorizontalSize = 0.35f;
+    private float walkingVerticalSize = 0.25f;
+
+    // Character Walk/Run
     private float horizontal;
+
     private bool isFacingRight = true;
 
     [SerializeField] private float speed = 4f;
@@ -16,33 +24,75 @@ public class Movement : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
+    // Character Sliding
+    private bool isSliding = false;
+    
+    // How long the character has been sliding
+    private float slideTimer = 0f;
+
+    // The maximum amount of time the character can slide for
+    [SerializeField] private float maxSlideTime = 1.5f;
+
     void Update()
+    {
+        checkUserInput();
+    }
+
+    // Fixed update is called at intervals
+    void FixedUpdate()
+    {
+        updateUserVelocity();
+    }
+
+    // Check for user input
+    void checkUserInput () 
     {
         // Check which direction the player should be moving
         horizontal = Input.GetAxisRaw("Horizontal");
 
+
+        // Flip the character's sprite if they are moving in the opposite direction of which they face
+        flipCharacterSprite();
+
         // Check if the 'w' or Space buttons are pressed down and if the user s on the ground
-        bool isButtonDown = Input.GetButtonDown("Jump");
-        if(isButtonDown && isGrounded())
+        if(Input.GetButtonDown("Jump") && isGrounded())
         {
             // Make the user jump
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpingPower);
         }
 
         // Check if the 'w' or Space button is not pressed down and if the user is moving upwards
-        bool isButtonUp = Input.GetButtonUp("Jump");
-        if (isButtonUp && rigidBody.velocity.y > 0f)
+        if (Input.GetButtonUp("Jump") && rigidBody.velocity.y > 0f)
         {
             // Make the user slowly lose momentum upwards
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y * 0.5f);
         }
 
-        // Flip the character's sprite if they are moving in the opposite direction of which they face
-        flip();
+        // Check if the 's' or Left Control buttons are pressed down
+        if(Input.GetButtonDown("Slide") && !isSliding) {
+            isSliding = true;
+            slideTimer = 0f;
+            // play slide animation
+            Vector2 size = characterCollider.size;
+            size.x = walkingVerticalSize;
+            size.y = walkingHorizontalSize;
+            characterCollider.size = size;
+        }
+
+        if(isSliding)
+        {
+            slideTimer += Time.deltaTime;
+            if(slideTimer > maxSlideTime) {
+                isSliding = false;
+                Vector2 size = characterCollider.size;
+                size.x = walkingHorizontalSize;
+                size.y = walkingVerticalSize;
+                characterCollider.size = size;
+            }
+        }
     }
 
-    // Fixed update is called at intervals
-    void FixedUpdate()
+    void updateUserVelocity()
     {
         // Make the user move in the direction they are indicating
         rigidBody.velocity = new Vector2(horizontal * speed, rigidBody.velocity.y);
@@ -55,7 +105,7 @@ public class Movement : MonoBehaviour
         return Physics2D.OverlapCircle(groundCheck.position, groundCheckRange, groundLayer);
     }
 
-    private void flip()
+    private void flipCharacterSprite()
     {
         // Check if the user is facing in the same direction that they're moving
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
